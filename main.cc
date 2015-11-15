a13c546bf2350df8eceba1ecfad1bd3e92f896ff
Centrixed <mattunderation@gmail.com>
2015-11-15 18:02:48 -0500

    Update

--- main.cc
+++ main.cc
@@ -9,16 +9,19 @@
 #include "simExcept.h"
 #include<iostream>
 #include<ctime>
+#include<queue>
+#include<stdlib.h>
 
 bool event(double); //function for checking events based on probability
 
 int main(int time_to_simulate/*, bool air_force_1*/){
 	int time = 1; 								//time loop in minutes for simulation
 	Airplane* airPlane = NULL;				//Airplane pointer for use in simulation
-	priority_queue<Airplane*> landingQ; //Queue for landing	
-	//Runway run1,run2,run3;				//three availible runways to land on	
-	bool planeArival = false;				//binary reference for plane arrival 
-	
+	priority_queue<Airplane*> landingQ; //Queue for landing
+	//Runway run1,run2,run3;				//three availible runways to land on
+	bool planeArival = false;				//binary reference for plane arrival
+	int minute = 0;
+
 	for(;time != time_to_simulate; minute++){ //time loop
 		planeArival = event(0.45); 				//check if a plane is arriving
 		if(planeArival){ 						 		//BEGIN new plane case
@@ -28,16 +31,17 @@ int main(int time_to_simulate/*, bool air_force_1*/){
 			}else
 				landingQ.push(airPlane);			//add new plane to landingQ
 		}													//END new plane case
-		
-		
-	
+
+
+
 	}
 
 
 }
 
 bool event(double prob){ //handles percentage values from 0 - 100%
-	int rnum = rand(time(NULL)) % 101; //generates number from 0 to 100
+    srand(time(NULL));
+	int rnum = rand() % 101; //generates number from 0 to 100
 	if(rnum <= prob*100)
 		return true;
 		return false;
